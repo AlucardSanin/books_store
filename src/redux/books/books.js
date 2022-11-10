bedd2d45a57628a27e03c0b95ebefa80850aabc5
AlucardSanin <alucardsanin@gmail.com>
2022-11-10 13:28:14 -0400

    Connect API for books

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,58 +1,50 @@
-const REMOVE = 'bookstore/books/REMOVE';
-const CREATE = 'bookstore/books/CREATE';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const defaultState = [
-  {
-    id: 1,
-    title: 'The Hunger Games',
-    genre: 'Action',
-    percentage: 64,
-    author: 'Suzanne Collins',
-    chapter: 17,
-  },
-  {
-    id: 2,
-    title: 'Dune',
-    genre: 'Sci-Fi',
-    percentage: 35,
-    author: 'Frank Herbert',
-    chapter: 3,
-  },
-  {
-    id: 3,
-    title: 'Capital in the Twenty-First Century',
-    genre: 'Economy',
-    percentage: 18,
-    author: 'Suzanne Collins',
-    chapter: 8,
-  },
-];
+const apiURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/WsLmIXPdjoycXsdgK4QM/books';
 
-// define an action creator for adding books
-export const addBook = (payload) => ({
-  type: CREATE,
-  payload,
-});
+const defaultState = [];
 
-// define an action creator for removing books
-export const removeBook = (id) => ({
-  type: REMOVE,
-  id,
-});
-
-export default function books(state = defaultState, action) {
-  const index = state.findIndex((object) => object.id === action.id);
-  switch (action.type) {
-    case CREATE:
-      return [...state, action.payload];
-
-    case REMOVE:
-      return [
-        ...state.slice(0, index),
-        ...state.slice(index + 1, state.length),
-      ];
-
-    default:
-      return state;
+export const addBook = createAsyncThunk('books/addBook', async (book) => {
+  const response = await axios.post(apiURL, book);
+  if (response.status === 201) {
+    return book;
   }
-}
+  return response.data;
+});
+
+export const getBooks = createAsyncThunk('books/getBooks', async () => {
+  const response = await axios.get(apiURL);
+  return response.data;
+});
+
+export const removeBook = createAsyncThunk('books/removeBook', async (book) => {
+  await axios.delete(`${apiURL}/${book.item_id}`);
+  return book;
+});
+
+export const booksSlice = createSlice({
+  name: 'books',
+  initialState: defaultState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // Add reducers to handle loading state as needed
+    builder
+      .addCase(addBook.fulfilled, (state, action) => {
+        // Add book
+        state.push(action.payload);
+      })
+      .addCase(getBooks.fulfilled, (state, action) => Object.entries(action.payload).map(
+        ([id, [book]]) => ({ ...book, item_id: id }),
+      ))
+      .addCase(removeBook.fulfilled, (state, action) => {
+        state.forEach((book) => {
+          if (book.item_id === action.payload.item_id) {
+            state.splice(state.indexOf(book), 1);
+          }
+        });
+      });
+  },
+});
+
+export default booksSlice.reducer;
